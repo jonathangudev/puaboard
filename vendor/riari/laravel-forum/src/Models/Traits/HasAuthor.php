81a2ca68f05a608e7fc51e3ee46952d274a3defe
johnguiscool <johnguiscool@gmail.com>
2019-07-22 21:13:40 -0400

    initial

---
+++ vendor/riari/laravel-forum/src/Models/Traits/HasAuthor.php
@@ -0,0 +1,35 @@
+<?php namespace Riari\Forum\Models\Traits;
+
+trait HasAuthor
+{
+    /**
+     * Relationship: Author.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        $model = config('forum.integration.user_model');
+        if (method_exists($model, 'withTrashed')) {
+            return $this->belongsTo($model, 'author_id')->withTrashed();
+        }
+
+        return $this->belongsTo($model, 'author_id');
+    }
+
+    /**
+     * Attribute: Author name.
+     *
+     * @return mixed
+     */
+    public function getAuthorNameAttribute()
+    {
+        $attribute = config('forum.integration.user_name');
+
+        if (!is_null($this->author)) {
+            return $this->author->$attribute;
+        }
+
+        return null;
+    }
+}
