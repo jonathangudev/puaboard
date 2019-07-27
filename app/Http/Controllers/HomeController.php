ed0d066d830b64e43a45ecbf8597a59803321349
johnguiscool <johnguiscool@gmail.com>
2019-07-27 16:17:28 -0400

    added route for creating FR

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\FieldReport;
+
 
 class HomeController extends Controller
 {
@@ -24,5 +26,17 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function saveFieldReport(Request $request)
+    {
+        $fieldReport = new FieldReport();
+        $fieldReport->title = $request->input('title');
+        $fieldReport->content = $request->input('content');
+        $fieldReport->user_id = auth()->user()->id;
+
+        $fieldReport->save();
+
+        return redirect('/home');
     }
 }
