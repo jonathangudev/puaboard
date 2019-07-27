c576ac278a9c480691f8d6019fae0ef259b57984
johnguiscool <johnguiscool@gmail.com>
2019-07-27 16:53:14 -0400

    experiment with vendor/forum/master.blade.php

--- resources/views/vendor/forum/master.blade.php
+++ resources/views/vendor/forum/master.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>
         @if (isset($thread))
-            {{ $thread->title }} -
+        {{ $thread->title }} -
         @endif
         @if (isset($category))
-            {{ $category->title }} -
+        {{ $category->title }} -
         @endif
         {{ trans('forum::general.home_title') }}
     </title>
@@ -22,92 +23,157 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
     <style>
-    body {
-        padding: 30px 0;
-    }
+        body {
+            padding: 30px 0;
+        }
 
-    textarea {
-        min-height: 200px;
-    }
+        textarea {
+            min-height: 200px;
+        }
 
-    .deleted {
-        opacity: 0.65;
-    }
+        .deleted {
+            opacity: 0.65;
+        }
     </style>
 </head>
-<body>
-    <div class="container">
-        @include ('forum::partials.breadcrumbs')
-        @include ('forum::partials.alerts')
 
-        @yield('content')
+<body>
+
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/forum">Forum</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Field Reports</a>
+                        </li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+
+            <div class="container">
+                @include ('forum::partials.breadcrumbs')
+                @include ('forum::partials.alerts')
+
+                @yield('content')
+            </div>
+
+        </main>
     </div>
 
     <script>
-    var toggle = $('input[type=checkbox][data-toggle-all]');
-    var checkboxes = $('table tbody input[type=checkbox]');
-    var actions = $('[data-actions]');
-    var forms = $('[data-actions-form]');
-    var confirmString = "{{ trans('forum::general.generic_confirm') }}";
+        var toggle = $('input[type=checkbox][data-toggle-all]');
+        var checkboxes = $('table tbody input[type=checkbox]');
+        var actions = $('[data-actions]');
+        var forms = $('[data-actions-form]');
+        var confirmString = "{{ trans('forum::general.generic_confirm') }}";
 
-    function setToggleStates() {
-        checkboxes.prop('checked', toggle.is(':checked')).change();
-    }
-
-    function setSelectionStates() {
-        checkboxes.each(function() {
-            var tr = $(this).parents('tr');
-
-            $(this).is(':checked') ? tr.addClass('active') : tr.removeClass('active');
-
-            checkboxes.filter(':checked').length ? $('[data-bulk-actions]').removeClass('hidden') : $('[data-bulk-actions]').addClass('hidden');
-        });
-    }
-
-    function setActionStates() {
-        forms.each(function() {
-            var form = $(this);
-            var method = form.find('input[name=_method]');
-            var selected = form.find('select[name=action] option:selected');
-            var depends = form.find('[data-depends]');
-
-            selected.each(function() {
-                if ($(this).attr('data-method')) {
-                    method.val($(this).data('method'));
-                } else {
-                    method.val('patch');
-                }
-            });
-
-            depends.each(function() {
-                (selected.val() == $(this).data('depends')) ? $(this).removeClass('hidden') : $(this).addClass('hidden');
-            });
-        });
-    }
-
-    setToggleStates();
-    setSelectionStates();
-    setActionStates();
-
-    toggle.click(setToggleStates);
-    checkboxes.change(setSelectionStates);
-    actions.change(setActionStates);
-
-    forms.submit(function() {
-        var action = $(this).find('[data-actions]').find(':selected');
-
-        if (action.is('[data-confirm]')) {
-            return confirm(confirmString);
+        function setToggleStates() {
+            checkboxes.prop('checked', toggle.is(':checked')).change();
         }
 
-        return true;
-    });
+        function setSelectionStates() {
+            checkboxes.each(function() {
+                var tr = $(this).parents('tr');
 
-    $('form[data-confirm]').submit(function() {
-        return confirm(confirmString);
-    });
+                $(this).is(':checked') ? tr.addClass('active') : tr.removeClass('active');
+
+                checkboxes.filter(':checked').length ? $('[data-bulk-actions]').removeClass('hidden') : $('[data-bulk-actions]').addClass('hidden');
+            });
+        }
+
+        function setActionStates() {
+            forms.each(function() {
+                var form = $(this);
+                var method = form.find('input[name=_method]');
+                var selected = form.find('select[name=action] option:selected');
+                var depends = form.find('[data-depends]');
+
+                selected.each(function() {
+                    if ($(this).attr('data-method')) {
+                        method.val($(this).data('method'));
+                    } else {
+                        method.val('patch');
+                    }
+                });
+
+                depends.each(function() {
+                    (selected.val() == $(this).data('depends')) ? $(this).removeClass('hidden'): $(this).addClass('hidden');
+                });
+            });
+        }
+
+        setToggleStates();
+        setSelectionStates();
+        setActionStates();
+
+        toggle.click(setToggleStates);
+        checkboxes.change(setSelectionStates);
+        actions.change(setActionStates);
+
+        forms.submit(function() {
+            var action = $(this).find('[data-actions]').find(':selected');
+
+            if (action.is('[data-confirm]')) {
+                return confirm(confirmString);
+            }
+
+            return true;
+        });
+
+        $('form[data-confirm]').submit(function() {
+            return confirm(confirmString);
+        });
     </script>
 
     @yield('footer')
 </body>
+
 </html>
