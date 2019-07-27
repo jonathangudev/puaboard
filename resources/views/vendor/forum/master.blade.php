aaeb508c12529155ca275c62fd0a08c9553bdce4
johnguiscool <johnguiscool@gmail.com>
2019-07-27 17:29:29 -0400

    no collapse

--- resources/views/vendor/forum/master.blade.php
+++ resources/views/vendor/forum/master.blade.php
@@ -44,11 +44,7 @@
         <nav class="navbar navbar-default navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
 
-                <button class="navbar-toggler" type="btn" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-left">
                         <li class="nav-item"><a class="navbar-brand" href="{{ url('/') }}">
