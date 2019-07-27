7f7e67c6411a8d01fb80057a3ef8105aca27e2c0
johnguiscool <johnguiscool@gmail.com>
2019-07-27 17:34:00 -0400

    stuff

--- resources/views/vendor/forum/master.blade.php
+++ resources/views/vendor/forum/master.blade.php
@@ -51,8 +51,9 @@
                             <strong><a class="nav-link " href="{{ url('/') }}">
                                     {{ config('app.name', 'Laravel') }}
                                 </a>
-                                <</strong>/li> <li class="nav-item">
-                                    <a class="nav-link" href="/home">Home</a>
+                            </strong>/li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/home">Home</a>
                         </li>
 
                         <li class="nav-item">
