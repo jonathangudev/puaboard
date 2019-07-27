ce592cfaf5d66f3e57f6e4bacfbe0f140adbf6ba
johnguiscool <johnguiscool@gmail.com>
2019-07-27 15:40:26 -0400

    added migration and create field report area

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function fieldReports()
+    {
+        return $this->hasMany('App\FieldReports');
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,39 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     You are logged in!
                 </div>
             </div>
         </div>
+
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Write a Field Report</div>
+
+                <div class="card-body">
+                    <form action="/field-report/create">
+                        <label for="title">Title</label>
+                        <input type="text" name="title">
+                        <label for="content">Content</label>
+                        <input type="text" name="content" rows="10">
+                        <input type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
