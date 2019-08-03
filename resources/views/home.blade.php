134b99469cdb9deab05c26e655c05f39bcab8074
johnguiscool <johnguiscool@gmail.com>
2019-08-03 14:04:06 -0400

    remove dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,22 +8,6 @@ $fieldReports = $allFieldReports;
 <div class="container">
     <div class="row justify-content-center">
 
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-
         <div class="col-md-8 mt-3">
             <div class="card">
                 <div class="card-header">Write a new Field Report</div>
