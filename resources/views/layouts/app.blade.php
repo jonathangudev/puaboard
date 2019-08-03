9ab762b2db77d6ebab9e45b8e2b95991a6befcc1
johnguiscool <johnguiscool@gmail.com>
2019-08-03 18:11:23 -0400

    tinymce 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>
+    <script src="http://cdn.tinymce.com/4/tinymce.min.js"></script>
     <script>
         tinymce.init({
             selector: 'textarea'
