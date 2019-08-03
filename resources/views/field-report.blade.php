bf25ab29801c59781e3bd64d5371014f834229a9
johnguiscool <johnguiscool@gmail.com>
2019-08-03 15:20:24 -0400

    delete button

--- resources/views/field-report.blade.php
+++ resources/views/field-report.blade.php
@@ -18,7 +18,7 @@
 
                             @if($hasPermissions)
                             <div class="container">
-                                <div class="row justify-content-center" data-toggle="modal" data-target="#exampleModal">
+                                <div class="row justify-content-center" data-toggle="modal" data-target="#deletionWarningModal">
                                     <button type="button" class="btn btn-danger">Delete</button>
                                 </div>
                             </div>
@@ -34,21 +34,24 @@
 
 @if($hasPermissions)
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="deletionWarningModal" tabindex="-1" role="dialog" aria-labelledby="deletionWarningModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="deletionWarningModalLabel">Field Report Deletion</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                Are you sure you want to delete this field report? It will not be possible to retrieve it afterwards.
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+
+                <form action="/field-report/{{$fieldReport->id}}" method="DELETE">
+                    <input type="button" class="btn btn-danger">Confirm Deletion</input>
+                </form>
             </div>
         </div>
     </div>
